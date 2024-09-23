880ca81aca2e847fe7f2964dc0d1efc024e611a8
livingstonm6 <94208409+livingstonm6@users.noreply.github.com>
2024-09-22 20:09:49 -0600

    Create PPM file.

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,54 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const image_width: u16 = 256;
+    const image_height: u16 = 256;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
+    // render
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout.print("P3\n{} {}\n255\n", .{ image_width, image_height });
 
-    try bw.flush(); // don't forget to flush!
+    for (0..image_height) |j| {
+        for (0..image_width) |i| {
+            const r: f64 = @as(f64, @floatFromInt(i)) / (image_width - 1);
+            const g: f64 = @as(f64, @floatFromInt(j)) / (image_height - 1);
+            const b: f64 = 0.0;
+
+            const ir: u32 = @as(u32, @intFromFloat(255.999 * r));
+            const ig: u32 = @as(u32, @intFromFloat(255.999 * g));
+            const ib: u32 = @as(u32, @intFromFloat(255.999 * b));
+
+            try stdout.print("{} {} {}\n", .{ ir, ig, ib });
+        }
+    }
+
+    // write to stdout, pipe into file
+
+    try bw.flush();
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+// pub fn main() !void {
+//     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+//     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+//     // stdout is for the actual output of your application, for example if you
+//     // are implementing gzip, then only the compressed bytes should be sent to
+//     // stdout, not any debugging messages.
+//     const stdout_file = std.io.getStdOut().writer();
+//     var bw = std.io.bufferedWriter(stdout_file);
+//     const stdout = bw.writer();
+
+//     try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+//     try bw.flush(); // don't forget to flush!
+// }
+
+// test "simple test" {
+//     var list = std.ArrayList(i32).init(std.testing.allocator);
+//     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+//     try list.append(42);
+//     try std.testing.expectEqual(@as(i32, 42), list.pop());
+// }
