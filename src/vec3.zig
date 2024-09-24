52e3f793f361d4ae952c363e8363189fc5703a10
livingstonm6 <livingstonm6@mymacewan.ca>
2024-09-24 13:51:33 -0600

    Debugged, refactored, and tested vec3.

--- src/vec3.zig
+++ src/vec3.zig
@@ -6,30 +6,58 @@ pub const vec3 = struct {
     z: f64,
 };
 
-pub fn vec3Add(v1: *vec3, v2: *vec3) vec3 {
-    return vec3{ .x = v1.x + v2.x, .y = v1.y + v2.y, .z = v2.z + v2.z };
+pub const point3 = vec3;
+
+pub fn add(v1: *const vec3, v2: *const vec3) vec3 {
+    return vec3{ .x = v1.x + v2.x, .y = v1.y + v2.y, .z = v1.z + v2.z };
 }
 
-pub fn vec3DotProduct(v1: *vec3, v2: *vec3) f64 {
+pub fn subtract(v1: *const vec3, v2: *const vec3) vec3 {
+    return vec3{ .x = v1.x - v2.x, .y = v1.y - v2.y, .z = v1.z - v2.z };
+}
+
+pub fn dotProduct(v1: *const vec3, v2: *const vec3) f64 {
     return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
 }
 
-pub fn vec3Multiply(v: *vec3, c: f64) vec3 {
+pub fn multiply(v: *const vec3, c: f64) vec3 {
     return vec3{ .x = v.x * c, .y = v.y * c, .z = v.z * c };
 }
 
-pub fn vec3Divide(v: *vec3, c: f64) vec3 {
+pub fn divide(v: *const vec3, c: f64) vec3 {
     return vec3{ .x = v.x / c, .y = v.y / c, .z = v.z / c };
 }
 
-pub fn vec3LengthSquared(v: *vec3) f64 {
-    return (v.x * v.x) + (v.y * v.y) + (v.z + v.z);
+pub fn lengthSquared(v: *const vec3) f64 {
+    return (v.x * v.x) + (v.y * v.y) + (v.z * v.z);
 }
 
-pub fn vec3Length(v: *vec3) f64 {
-    return std.math.sqrt(vec3LengthSquared(v));
+pub fn length(v: *const vec3) f64 {
+    return std.math.sqrt(lengthSquared(v));
 }
 
-pub fn vec3Unit(v: *vec3) vec3 {
-    return vec3Divide(v, vec3Length(v));
+pub fn unit(v: *const vec3) vec3 {
+    return divide(v, length(v));
+}
+
+test "add" {
+    const v1 = vec3{ .x = 1, .y = 1, .z = 1 };
+    const v2 = vec3{ .x = 1, .y = 2, .z = 3 };
+    const v3 = vec3{ .x = 2, .y = 3, .z = 4 };
+    const result = add(&v1, &v2);
+
+    try std.testing.expectEqual(result.x, v3.x);
+    try std.testing.expectEqual(result.y, v3.y);
+    try std.testing.expectEqual(result.z, v3.z);
+}
+
+test "subtract" {
+    const v1 = vec3{ .x = 1, .y = 1, .z = 1 };
+    const v2 = vec3{ .x = 1, .y = 2, .z = 3 };
+    const v3 = vec3{ .x = 0, .y = 1, .z = 2 };
+    const result = subtract(&v2, &v1);
+
+    try std.testing.expectEqual(result.x, v3.x);
+    try std.testing.expectEqual(result.y, v3.y);
+    try std.testing.expectEqual(result.z, v3.z);
 }
