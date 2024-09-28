9cde49e534367d3182f2d64568a029b65f27ad39
livingstonm6 <94208409+livingstonm6@users.noreply.github.com>
2024-09-28 12:44:41 -0600

    Implemented gamma correction.

--- src/color.zig
+++ src/color.zig
@@ -6,10 +6,17 @@ pub const color = v.vec3;
 
 const intensity = interval.Interval{ .min = 0.000, .max = 0.999 };
 
+fn linearToGamma(linear_component: f64) f64 {
+    if (linear_component > 0) {
+        return std.math.sqrt(linear_component);
+    }
+    return 0.0;
+}
+
 pub fn writeColor(w: anytype, pixel_color: *const color) !void {
-    const r = pixel_color.x;
-    const g = pixel_color.y;
-    const b = pixel_color.z;
+    const r = linearToGamma(pixel_color.x);
+    const g = linearToGamma(pixel_color.y);
+    const b = linearToGamma(pixel_color.z);
 
     const ir: u16 = @as(u16, @intFromFloat(256 * intensity.clamp(r)));
     const ig: u16 = @as(u16, @intFromFloat(256 * intensity.clamp(g)));
