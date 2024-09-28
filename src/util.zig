a0543b8b9116b72286ed38bf0687ab06be7e42de
livingstonm6 <94208409+livingstonm6@users.noreply.github.com>
2024-09-27 19:08:18 -0600

    Added util.zig with functions to get random floats.

---
+++ src/util.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub fn randomF64() !f64 {
+    var prng = std.rand.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.posix.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+
+    const rand = prng.random();
+
+    return rand.float(f64);
+}
+
+pub fn randomF64Range(min: f64, max: f64) f64 {
+    return min + ((max - min) * randomF64());
+}
