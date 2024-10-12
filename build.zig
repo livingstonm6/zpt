eb1d01b91af2c7ee100916c22ba1d14e84a14d54
livingstonm6 <livingstonm6@mymacewan.ca>
2024-10-11 21:08:44 -0600

    Added zstbi dependency.

--- build.zig
+++ build.zig
@@ -36,6 +36,10 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const zstbi = b.dependency("zstbi", .{});
+    exe.root_module.addImport("zstbi", zstbi.module("root"));
+    exe.linkLibrary(zstbi.artifact("zstbi"));
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
