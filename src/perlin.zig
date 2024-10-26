6211a0c18750ba133bfcf29b5fa49f0fdda0efd7
livingstonm6 <livingstonm6@mymacewan.ca>
2024-10-26 15:34:19 -0600

    Implemented Perlin interpolation with random unit vec3s instead of f64s.

--- src/perlin.zig
+++ src/perlin.zig
@@ -18,9 +18,32 @@ fn trilinearInterpret(c: [2][2][2]f64, u: f64, v: f64, w: f64) f64 {
     return accum;
 }
 
+fn perlinInterpret(c: [2][2][2]vec.vec3, u: f64, v: f64, w: f64) f64 {
+    const uu = u * u * (3 - 2 * u);
+    const vv = v * v * (3 - 2 * v);
+    const ww = w * w * (3 - 2 * w);
+
+    var accum: f64 = 0.0;
+
+    for (0..2) |i| {
+        for (0..2) |j| {
+            for (0..2) |k| {
+                const f_i: f64 = @floatFromInt(i);
+                const f_j: f64 = @floatFromInt(j);
+                const f_k: f64 = @floatFromInt(k);
+
+                const weight = vec.vec3{ .x = u - f_i, .y = v - f_j, .z = w - f_k };
+                accum += (f_i * uu + (1 - f_i) * (1 - uu)) * (f_j * vv + (1 - f_j) * (1 - vv)) * (f_k * ww + (1 - f_k) * (1 - ww)) * vec.dotProduct(&c[i][j][k], &weight);
+            }
+        }
+    }
+
+    return accum;
+}
+
 pub const Perlin = struct {
     point_count: usize = 256,
-    rand: []f64 = undefined,
+    rand: []vec.vec3 = undefined,
     perm_x: []usize = undefined,
     perm_y: []usize = undefined,
     perm_z: []usize = undefined,
@@ -47,13 +70,13 @@ pub const Perlin = struct {
     }
 
     pub fn init(self: *Perlin, allocator: std.mem.Allocator) !void {
-        self.rand = try allocator.alloc(f64, self.point_count);
+        self.rand = try allocator.alloc(vec.vec3, self.point_count);
         self.perm_x = try allocator.alloc(usize, self.point_count);
         self.perm_y = try allocator.alloc(usize, self.point_count);
         self.perm_z = try allocator.alloc(usize, self.point_count);
 
         for (0..self.point_count) |i| {
-            self.rand[i] = try util.randomF64();
+            self.rand[i] = vec.unit(&try vec.randomRange(-1, 1));
         }
 
         try self.generatePerm(self.perm_x);
@@ -71,29 +94,15 @@ pub const Perlin = struct {
     }
 
     pub fn noise(self: Perlin, p: *const vec.point3) !f64 {
-        var u = p.x - @floor(p.x);
-        var v = p.y - @floor(p.y);
-        var w = p.z - @floor(p.z);
-
-        // Hermitian smoothing
-        u = u * u * (3 - 2 * u);
-        v = v * v * (3 - 2 * v);
-        w = w * w * (3 - 2 * w);
+        const u = p.x - @floor(p.x);
+        const v = p.y - @floor(p.y);
+        const w = p.z - @floor(p.z);
 
         const i = @as(isize, @intFromFloat(@floor(p.x)));
         const j = @as(isize, @intFromFloat(@floor(p.y)));
         const k = @as(isize, @intFromFloat(@floor(p.z)));
 
-        var c = [2][2][2]f64{
-            [2][2]f64{
-                [2]f64{ 0, 0 },
-                [2]f64{ 0, 0 },
-            },
-            [2][2]f64{
-                [2]f64{ 0, 0 },
-                [2]f64{ 0, 0 },
-            },
-        };
+        var c: [2][2][2]vec.vec3 = undefined;
 
         for (0..2) |di| {
             for (0..2) |dj| {
@@ -113,6 +122,6 @@ pub const Perlin = struct {
             }
         }
 
-        return trilinearInterpret(c, u, v, w);
+        return perlinInterpret(c, u, v, w);
     }
 };
